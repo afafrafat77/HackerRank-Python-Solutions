17970d27faa651aeed4002824bca4508253a9d74
Afaf Rafat Ibrahim <85519875+afafrafat77@users.noreply.github.com>
2024-11-28 06:46:59 +0200

    Create Loops.py

---
+++ Easy/Loops.py
@@ -0,0 +1,6 @@
+# Print the squares of all non-negative integers less than the given integer.
+# For each integer from 0 to n-1, output its square on a new line.
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(n):
+        print(i*i)
