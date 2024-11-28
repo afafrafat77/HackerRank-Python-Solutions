011c98a3ac6bc7bd2c7c74cf7b6df4369f1a81f9
Afaf Rafat Ibrahim <85519875+afafrafat77@users.noreply.github.com>
2024-11-28 06:46:05 +0200

    Create Python: Division.py

---
+++ Easy/Python: Division.py
@@ -0,0 +1,7 @@
+# Perform integer and float division of two input integers and print the results.
+# The first line outputs the result of integer division, and the second line outputs float division.
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print (int(a/b))
+    print(a/b)
