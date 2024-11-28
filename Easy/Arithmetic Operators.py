b981923df93605a82171400ada51e73de4706fc2
Afaf Rafat Ibrahim <85519875+afafrafat77@users.noreply.github.com>
2024-11-28 06:45:06 +0200

    Create Arithmetic Operators.py

---
+++ Easy/Arithmetic Operators.py
@@ -0,0 +1,8 @@
+# Perform arithmetic operations (sum, difference, product) on two integers and print the results.
+# Input two integers, then output their sum, difference, and product on separate lines.
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
