e3fc58135839f2b4ff11c34336cf12d4b4d199ab
Afaf Rafat Ibrahim <85519875+afafrafat77@users.noreply.github.com>
2024-11-28 06:59:29 +0200

    Create What's Your Name?.py

---
+++ Easy/What's Your Name?.py
@@ -0,0 +1,14 @@
+# Given a first name and a last name, print a greeting message with the full name.
+# The output should be in the format: 'Hello firstname lastname! You just delved into python.'
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(first, last):
+    # Write your code here
+    print("Hello "+first+" "+last+"! You just delved into python.")
